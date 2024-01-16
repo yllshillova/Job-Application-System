34dd4ed2e9bdb78e94eff81f29d3d96b437ed4e8
RiinorS <rs57306@ubt-uni.net>
2024-01-16 19:57:22 +0100

    Started the configuration of the token service

---
+++ Application/Services/AccountServices/TokenService.cs
@@ -0,0 +1,30 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Domain;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Application.Services.AccountServices;
+
+public class TokenService
+{
+    public string CreateToken(User user)
+    {
+        var claims = new List<Claim>{
+            new Claim(ClaimTypes.Name, user.UserName),
+            new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            new Claim(ClaimTypes.Email, user.Email),
+
+
+        };
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("super secret key"));
+        var creds = new SigningCredentials(key , SecurityAlgorithms.HmacSha512Signature);
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.UtcNow.AddDays(7),
+            SigningCredentials = creds
+        };
+        var tokenHandler = new JwtSecurityTokenHandler();
+    }
+}
