abf065ef779490371c777231a41eaaf4e4440b5b
RiinorS <rs57306@ubt-uni.net>
2024-01-18 01:57:15 +0100

    Added validation for the registration of users

--- Domain/DTOs/AccountDTOs/RegisterDto.cs
+++ Domain/DTOs/AccountDTOs/RegisterDto.cs
@@ -1,12 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Domain.DTOs.AccountDTOs;
 
 public class RegisterDto
 {
+    [Required]
     public string UserName { get; set; }
-
+    [Required]
     public string UserLastName { get; set; }
-
+    [Required]
+    [EmailAddress]
     public string Email { get; set; }
+    [Required]
+    [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$",
+     ErrorMessage ="Password must be at minimum 4 to maximum 8 characters, and must include a number, lower and uppercase letter ")]
     public string Password { get; set; }
 
 }
